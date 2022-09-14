d6b1b00d6a462ef56460e0e282b2c9506a342688
“Ravi <patilravi400@gmail.com>
2022-09-14 09:48:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import ProductContainer from './Components/ProductContainer';
+import PureComponent from './Components/PureComponent';
+import './App.css'
+
+class App extends Component {
+ render(){
+  return (
+    <div className='App'>
+      <ProductContainer/>
+      <PureComponent />
+
+    </div>
+  )
+ }
+}
+export default App;
+
+
+
